1328ac2a7bdd6478c36bf90714917c1afb93bbdf
Ar3secchim <renarasecchim@gmail.com>
2023-12-01 09:21:33 -0300

    refactor: replace imports and injections dependecie

--- src/main/java/ada/tech/tenthirty/tvpackages/application/service/GetSubscriptionById.java
+++ src/main/java/ada/tech/tenthirty/tvpackages/application/service/GetSubscriptionById.java
@@ -2,16 +2,16 @@ package ada.tech.tenthirty.tvpackages.application.service;
 
 import ada.tech.tenthirty.tvpackages.domain.Subscription;
 import ada.tech.tenthirty.tvpackages.infra.repository.SubscriptionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class GetSubscriptionById {
-  @Autowired
-  private SubscriptionRepository subscriptionRepository;
+  private final SubscriptionRepository subscriptionRepository;
 
   public Subscription execute(String subId) {
     return subscriptionRepository.findById(subId).orElseThrow();
   }
-
 }
--- src/main/java/ada/tech/tenthirty/tvpackages/application/service/HttpPayment.java
+++ src/main/java/ada/tech/tenthirty/tvpackages/application/service/HttpPayment.java
@@ -3,13 +3,9 @@ package ada.tech.tenthirty.tvpackages.application.service;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.Map;
